63db7fcd7f7693c1e5cb27970032c14ff7a58122
Billy Kim <billyhkim.dev@gmail.com>
2019-09-25 14:33:27 -0700

    added dependencies and initial app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => res.send('INDEX'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
