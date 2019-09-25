a8024dd2b55700a75681e8686b38f3e27f9c2be5
Billy Kim <billyhkim.dev@gmail.com>
2019-09-25 15:03:57 -0700

    importing Gig routes

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const app = express();
 
 app.get('/', (req, res) => res.send('INDEX'));
 
+// Gig routes
+app.use('/gigs', require('./routes/gigs'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
