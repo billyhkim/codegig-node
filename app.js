1ca619b318118a22e127478a93b5690b659ae4e3
Billy Kim <billyhkim.dev@gmail.com>
2019-09-25 14:39:21 -0700

    import database code

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+// Database
+const db = require('./config/database');
+
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
+
 const app = express();
 
 app.get('/', (req, res) => res.send('INDEX'));
