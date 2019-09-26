4f8b5bf90d533c7902fbf2b14fbf7b7bb549b767
Billy Kim <billyhkim.dev@gmail.com>
2019-09-26 10:45:14 -0700

    adding new table data thru routes/gigs

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+// Get gig list
 router.get('/', (req, res) => 
   Gig.findAll()
   .then(gigs => {
@@ -10,5 +11,29 @@ router.get('/', (req, res) =>
     res.sendStatus(200);
   })
   .catch(err => console.log(err)));
+
+// Add a gig
+router.get('/add', (req, res) => {
+  const data = {
+    title: 'Looking for a React developer',
+    technologies: 'React, JavaScript, HTML, CSS',
+    budget: '$3,000',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lobortis consequat leo, id condimentum lacus. Nam blandit turpis eros, quis viverra tortor semper in. In nisi lorem, feugiat id fringilla et, scelerisque nec lacus. Nam hendrerit, urna vel eleifend fringilla, diam mauris gravida neque, sit amet condimentum turpis lacus ut leo. Sed aliquam fringilla imperdiet. Nam massa nisi, feugiat nec arcu nec, fringilla pulvinar augue.',
+    contact_email: 'user1@gmail.com'
+  }
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  // Insert into table
+  Gig.create({
+    title,
+    technologies,
+    description,
+    budget,
+    contact_email
+  })
+    .then(gig => res.redirect('/gigs'))
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
