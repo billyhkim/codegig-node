eec54b3475be2084cd126a9f554f05d2ac331245
Billy Kim <billyhkim.dev@gmail.com>
2019-09-26 11:01:34 -0700

    add handlebars and set static folder

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ db.authenticate()
 
 const app = express();
 
+// Handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => res.send('INDEX'));
 
 // Gig routes
