00b9382982c804db2c8880e72b388bf05e9222fd
Billy Kim <billyhkim.dev@gmail.com>
2019-09-27 11:06:08 -0700

    testing adding additional rows from routes/gigs

--- routes/gigs.js
+++ routes/gigs.js
@@ -7,8 +7,9 @@ const Gig = require('../models/Gig');
 router.get('/', (req, res) => 
   Gig.findAll()
   .then(gigs => {
-    console.log(gigs);
-    res.sendStatus(200);
+    res.render('gigs', {
+      gigs
+    });
   })
   .catch(err => console.log(err)));
 
